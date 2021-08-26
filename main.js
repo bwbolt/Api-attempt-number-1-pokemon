f216c833f11a2db4aaf1e3c9d7223d40a683f9fc
Bryce Wein <bryce.wein@gmail.com>
2021-08-25 20:28:40 -0400

    Generated Simple Api
    Not much to see. I set up and HTML, css, and javascript doc and made a simple api test app to see what it is all about

---
+++ main.js
@@ -0,0 +1,32 @@
+const apiData = {
+  url: 'https://pokeapi.co/api/v2/',
+  type: 'pokemon',
+  id: '26',
+
+}
+
+
+
+const apiUrl = `${apiData.url}${apiData.type}/${apiData.id}`
+
+fetch(apiUrl)
+      .then( (data) => data.json() )
+      .then( (pokemon)=> generateHtml(pokemon))
+
+
+const generateHtml =(data)=>{
+  console.log(data)
+  const html = `
+    <div class="name">${data.name}</div>
+    <img src=${data.sprites.front_default}>
+    <div class="details">
+      <span>Height:${data.height}</span>
+      <span>Weight: ${data.weight}</span>
+    </div>    
+  `
+
+
+  const pokemonDiv = document.querySelector('.pokemon')
+  pokemonDiv.innerHTML= html
+
+}
